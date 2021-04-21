ce4b3de7f0e652869c499965ea1d38b48f5c8b82
yuanchuan <yuanchuan23@gmail.com>
2021-04-21 13:22:02 +0800

    ship

---
+++ README.md
@@ -0,0 +1,73 @@
+# vite-plugin-autoimport
+
+Automatically detect and import components or modules.
+
+<img src="screenshot.png" alt="autoimport screenshot" />
+
+## Motivations
+
+It's very common to have many components in one file as the project grows,
+and several frequently-used modules may be imported in almost every file.
+Therefore, this plugin is for reducing the code of imports. You can treat those
+components as global but they're being injected locally whenever required.
+
+
+## Feature
+
+* Can detect new created components without restart the dev server.
+
+## All options
+
+```js
+const { autoImport } from 'vite-plugin-autoimport';
+
+// svelte.config.cjs
+vite: {
+  plugins: [
+    autoImport({
+
+      // where to search for the components
+      components: [
+        './src/components',
+        './src/routes/_fragments',
+      ],
+
+      // some frequently used modules
+      module: {
+        svelte: ['onMount', 'createEventDispatcher']
+      },
+
+      // manually import
+      mapping: {
+        API:  `import API from './src/api'`,
+        Icon: `import * as Icon from '$lib/icon'`,
+      },
+
+      // autoimport only for .svelte files
+      include: ['**/*.svelte'],
+
+      // node_modules is ignored by default
+      exclude: ['node_modules'],
+
+    })
+  ]
+}
+```
+
+## Name conventions
+
+Component names will be normalized to **upper camel case**, for example:
+
+```js
+import MyComponent from '../my-component.svelte'
+import MyAnotherComponent from '../my_another_component.svelte'
+```
+
+## Note
+
+I only use it for `SvelteKit` projects. PRs are welcome for adding support to `vue`.
+
+
+## TODO
+
+- Add sourcemap
---
+++ index.js
@@ -0,0 +1,164 @@
+const fs = require('fs');
+const path = require('path');
+const { walk } = require('estree-walker');
+const { attachScopes, createFilter } = require('@rollup/pluginutils');
+
+exports.autoImport = autoImport;
+
+/**
+ * @param {string|string[]} [components] - Component paths
+ * @param {object} [module] - Mapping exported method/property from modules
+ * @param {object} [mapping] - Set mapping manually
+ * @param {string|string[]} [include]
+ * @param {string|String[]} [exclude]
+ */
+function autoImport({ components, module, mapping, include, exclude } = {}) {
+  const filter = createFilter(include, exclude);
+  let importMapping = {};
+
+  const updateMapping = () => {
+    importMapping = createMapping({ components, module, mapping, filter });
+  };
+
+  updateMapping();
+
+  return {
+    name: 'autoImport',
+    transform: function(code, filePath) {
+      if (!filter(filePath) || /\/node_modules/.test(filePath)) {
+        return null;
+      }
+      const ast = this.parse(code);
+      const scope = attachScopes(ast, 'scope');
+      const imported = new Set();
+      walk(ast, {
+        enter(node) {
+          if (node.type === 'ImportDeclaration') {
+            node.specifiers.forEach(sf => {
+              imported.add(sf.local.name);
+            });
+          }
+        }
+      });
+      Object.entries(importMapping).forEach(([name, value]) => {
+        if (/\W/.test(name)) {
+          return false;
+        }
+        if (imported.has(name) || scope.contains(name)) {
+          return false;
+        }
+        if (code.includes(name)) {
+          let importValue = (typeof value == 'function')
+            ? value(path.dirname(filePath))
+            : value;
+          code = prependTo(code, importValue);
+        }
+      });
+      return code;
+    },
+    configureServer(server) {
+      let componentPaths = makeArray(components).map(n => path.resolve(n));
+      if (componentPaths.length) {
+        server.watcher
+          .add(componentPaths)
+          .on('add', updateMapping)
+          .on('unlink', updateMapping);
+      }
+    }
+  }
+}
+
+function createMapping({ components, module, mapping, filter }) {
+  const importMapping = {};
+
+  // Read all components from given paths
+  // and transform the import names into CamelCase
+  makeArray(components).forEach(comp => {
+    let componentPath = path.resolve(comp);
+    traverse(componentPath, name => {
+      if (!filter(name)) {
+        return false;
+      }
+      let parsed = path.parse(name);
+      let moduleName = camelize(parsed.name);
+      if (parsed.name === 'index') {
+        moduleName = camelize(getLastDir(parsed.dir));
+      }
+      importMapping[moduleName] = target => {
+        let moduleFrom = path.relative(target, name);
+        if (!moduleFrom.startsWith('.')) {
+          moduleFrom = './' + moduleFrom;
+        }
+        return `import ${moduleName} from '${moduleFrom}'`
+      }
+    });
+  });
+
+  // Select methods or properties from a given module
+  Object.entries(makeLiteral(module)).forEach(([moduleFrom, names]) => {
+    makeArray(names).forEach(name => {
+      importMapping[name] = `import { ${name} } from '${moduleFrom}'`;
+    });
+  });
+
+  // Custom mapping is useful for overwriting and
+  // import things other than components
+  Object.assign(importMapping, makeLiteral(mapping));
+
+  return importMapping;
+}
+
+function makeArray(arr) {
+  if (Array.isArray(arr)) {
+    return arr;
+  }
+  if (arr !== undefined && arr !== null) {
+    return [];
+  }
+  return [arr];
+}
+
+function makeLiteral(obj) {
+  if (obj && obj.constructor === Object) {
+    return obj;
+  }
+  return {};
+}
+
+function prependTo(code, injection) {
+  return '\n' + injection + '\n' + code;
+}
+
+function toUpperCase(_, c) {
+  return String(c).toUpperCase();
+}
+
+function camelize(name) {
+  return name
+    .replace(/[-_]+(.{1})/g, toUpperCase)
+    .replace(/^(.{1})/, toUpperCase);
+}
+
+function getLastDir(dir) {
+  let dirs = dir.split(path.sep).filter(n => n !== 'index');
+  return dirs[dirs.length - 1];
+}
+
+function traverse(root, fn) {
+  if (!fs.existsSync(root)) {
+    return false;
+  }
+  if (!fs.statSync(root).isDirectory()) {
+    return false;
+  }
+  let originRoot = root;
+  fs.readdirSync(root).forEach(dir => {
+    dir = path.join(originRoot, dir);
+    let stat = fs.statSync(dir);
+    if (stat.isDirectory()) {
+      traverse(dir, fn);
+    } else if (stat.isFile()) {
+      fn(dir);
+    }
+  });
+}

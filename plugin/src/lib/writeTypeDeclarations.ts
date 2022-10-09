a5357ea933eead417a4167a6580e03a7019602f4
Loris Sigrist <43482866+LorisSigrist@users.noreply.github.com>
2022-10-09 12:25:04 +0200

    Create .svelte-kit directory, if it does not yet exist

--- plugin/src/lib/writeTypeDeclarations.ts
+++ plugin/src/lib/writeTypeDeclarations.ts
@@ -1,16 +1,18 @@
 import path from 'path'
-import {writeFileSync} from 'fs'
+import {writeFileSync, mkdirSync} from 'fs'
 import { TypeDeclarationMapping } from '../types.js'
 
-const PATH_TO_DTS='./.svelte-kit/'
+const DTS_DIR='./.svelte-kit/'
 const DTS_NAME='components.d.ts'
 
 export function writeTypeDeclarations(typeDeclarations: TypeDeclarationMapping) {
     let declarationStatements : string[] = [];
 
     Object.values(typeDeclarations).forEach( getDeclaration => {
-        declarationStatements.push(getDeclaration(PATH_TO_DTS));
+        declarationStatements.push(getDeclaration(DTS_DIR));
     })
 
-    writeFileSync(path.resolve(PATH_TO_DTS + DTS_NAME), declarationStatements.join('\n'))
+    //Creating the directory, if it does not already exist. This is harmless in case it already does exist
+    mkdirSync(path.resolve(DTS_DIR), { recursive: true });
+    writeFileSync(path.resolve(DTS_DIR + DTS_NAME), declarationStatements.join('\n'))
 }
